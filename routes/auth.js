f7a4967b3737479ff9cb32a86e541eb9e96d3720
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-09 20:44:18 +0700

    Catching Error

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,15 @@ const authRouter = express.Router();
 authRouter.post("/api/signup", async (req, res) => {
   try {
     const { fullname, email, password } = req.body;
-    const existingEmail = await User.findOne({ email: email });
+    const existingEmail = await User.findOne({ email: email }); // Check if the email already exists
     if (existingEmail) {
-      return res.status(400).json({ message: "Email already exists" });
+      return res.status(400).json({ message: "Email already exists" }); // Return an error message if the email already exists
     } else {
-      var user = new User({ fullname, email, password });
-      user = await user.save();
-      res.json({ user });
+      let user = new User({ fullname, email, password }); // Create a new user object
+      user = await user.save(); // Save the user to the database
+      res.json({ user }); // Return the user object
     }
-  } catch (error) {}
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
