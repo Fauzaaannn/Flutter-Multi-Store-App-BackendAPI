b5bf4e3e982320fe4205102cd1fc556dc16695b4
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-09 18:58:29 +0700

    Validation Check

--- models/user.js
+++ models/user.js
@@ -11,6 +11,14 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    validate: {
+      validator: (value) => {
+        const result =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return result.test(value);
+      },
+      message: "Please enter a valid email address",
+    },
   },
 
   state: {
@@ -31,6 +39,13 @@ const userSchema = mongoose.Schema({
   password: {
     type: String,
     required: true,
+    validate: {
+      validator: (value) => {
+        // Check if the password is at least 8 characters long
+        return value.length >= 8;
+      },
+      message: "Password must be at least 8 characters long",
+    },
   },
 });
 
