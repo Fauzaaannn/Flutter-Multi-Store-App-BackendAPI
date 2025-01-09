77ea4993db0a55afb0e5961f38716a1e96878008
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-09 14:25:52 +0700

    Flutter Store Backend API with MongoDB Connection

---
+++ index.js
@@ -0,0 +1,25 @@
+// import the express module
+const express = require("express");
+const routes = require("./routes/hello");
+const mongoose = require("mongoose");
+
+// Define the port number
+const port = 3000;
+
+// Create an instance of the express application
+const app = express();
+// Connect to MongoDB String
+const DB =
+  "mongodb+srv://fauzanabderrasheed:OjanMongoAtlas@cluster0.mci0f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Middleware
+app.use(routes);
+
+mongoose.connect(DB).then(() => {
+  console.log("MongoDB Connection Successful");
+});
+
+// start the server and listen on specified port
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
