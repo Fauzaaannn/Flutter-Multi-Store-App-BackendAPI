b555965a2fb120ee15b2a33285d6f14a1c1ef454
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-15 20:44:55 +0700

    Making Post API Route Endpoint

---
+++ routes/category.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Category = require("../models/category");
+
+const categoryRouter = express.Router();
+
+categoryRouter.post("/api/categories", async (req, res) => {
+    try {
+        const { name, image, banner } = req.body; // Destructuring the name, image, and banner from the request body
+        const category = new Category({ name, image, banner }); // Create a new instance of the Category model
+        await category.save(); // Save the category to the database
+        return res.status(201).send(category); // Return the saved category
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
+});
+
+module.exports = categoryRouter;
