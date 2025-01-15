608c6ecc9ebc69ba864f267b6cd01b581da0feb0
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-15 21:14:31 +0700

    GET Route API Endpoint

--- routes/category.js
+++ routes/category.js
@@ -4,14 +4,23 @@ const Category = require("../models/category");
 const categoryRouter = express.Router();
 
 categoryRouter.post("/api/categories", async (req, res) => {
-    try {
-        const { name, image, banner } = req.body; // Destructuring the name, image, and banner from the request body
-        const category = new Category({ name, image, banner }); // Create a new instance of the Category model
-        await category.save(); // Save the category to the database
-        return res.status(201).send(category); // Return the saved category
-    } catch (e) {
-        res.status(500).send({ error: e.message });
-    }
+  try {
+    const { name, image, banner } = req.body; // Destructuring the name, image, and banner from the request body
+    const category = new Category({ name, image, banner }); // Create a new instance of the Category model
+    await category.save(); // Save the category to the database
+    return res.status(201).send(category); // Return the saved category
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+});
+
+categoryRouter.get("/api/categories", async (req, res) => {
+  try {
+    const categories = await Category.find(); // Find all categories in the database
+    res.status(200).json({ categories }); // Return the list of categories
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
 module.exports = categoryRouter;
