7de291e0d73d6a45e325e44e90d79b1ae9ce9c05
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-17 16:48:14 +0700

    Get Reviews and Rating - Get Request

--- routes/product_review.js
+++ routes/product_review.js
@@ -21,5 +21,13 @@ productReviewRouter.post("/api/product-review", async (req, res) => {
   }
 });
 
+productReviewRouter.get("/api/reviews", async (req, res) => {
+  try {
+    const reviews = await ProductReview.find({});
+    res.status(200).json({ reviews });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 module.exports = productReviewRouter;
