db2bb834a79ec174d72ec4c490bd6a342bff7344
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-17 14:16:27 +0700

    Product Schema

---
+++ models/product.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  productPrice: {
+    type: Number,
+    required: true,
+  },
+
+  quantity: {
+    type: Number,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+
+  subCategory: {
+    type: String,
+    required: true,
+  },
+
+  images: [ // to store array of images
+    {
+      type: String,
+      required: true,
+    },
+  ],
+
+  popular: {
+    type: Boolean,
+    default: true,
+  },
+
+  recommend: {
+    type: Boolean,
+    default: false,
+  },
+
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
