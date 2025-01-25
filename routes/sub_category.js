3d74dcb5032709d6da2a12c752f59f489613fc94
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-25 16:13:04 +0700

    Adding GET all subcategories

--- routes/sub_category.js
+++ routes/sub_category.js
@@ -19,6 +19,15 @@ subcategoryRouter.post("/api/subcategories", async (req, res) => {
   }
 });
 
+subcategoryRouter.get("/api/subcategories", async (req, res) => {
+  try {
+    const subcategories = await SubCategory.find({}); // Find all subcategories in the database
+    return res.status(200).json(subcategories); // Return the list of subcategories
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 subcategoryRouter.get(
   "/api/category/:categoryName/subcategories",
   async (req, res) => {
